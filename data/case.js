304a333fa654b5cb3ac33737e0239af8368121b5
carol02 <1057889038@qq.com>
2016-12-12 14:18:17 +0800

    person

---
+++ data/case.js
@@ -0,0 +1,427 @@
+
+/*		前端知识汇总&案例展示
+	*   {
+	*       caseName: @value String            		*         案例总标题
+	*       caseList: @value Array         			*         案例说明信息
+	*       	caseTitle: @value String            *         案例标题  
+	*      		publishTime: @value String 			*         案例上传时间  格式为：2016.10.09 08:00
+	*       	caseThumbnail: @value String        *         缩略图地址
+	*       	caseDescription: @value String      *         案例描述 
+				caseWebsite: @value String       	*         案例网址
+	*       
+	*   }
+*/
+
+
+
+
+var casees = [
+		{
+			caseName:"JS的属性操作",
+			caseList:[
+				{
+					caseTitle:"JS热身运动练习",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/1-JS热身课程-课后练习.gif",
+					caseDescription:"属性操作、图片切换、短信属性操作<br>图片切换、短信",
+					caseWebsite:"demo/case1/改变div样式.html"
+				},
+				{
+					caseTitle:"模拟手机短信发送",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/2-模拟手机短信发送.gif",
+					caseDescription:"描述：这是我完成的一个小案例，综合涉及到的知识点是判断、字符串拼接，以及布局方面的小技巧，比如提交留言后，A用户在左，B用户在右"
+					,caseWebsite:"demo/case1/模拟手机发短信/模拟手机短信.html"
+				},
+				{
+					caseTitle:"输入数值改变样式",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/1.png",
+					caseWebsite:"demo/case1/输入数值改变样式.html"
+				},
+				{
+					caseTitle:"图片切换",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/2.png",
+					caseWebsite:"demo/case1/图片切换/图片切换.html"
+				},
+				{
+					caseTitle:"字体放大缩小",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/3.png",
+					caseWebsite:"demo/case1/字体放大缩小.html"
+				}
+			]
+		},
+		{
+			caseName:"for应用this关键字",
+			caseList:[
+				{
+					caseTitle:"弹出小图",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/4.png",
+					caseDescription:"鼠标移入显示小图",
+					caseWebsite:"demo/case2/新闻列表/弹出层.html"
+				},
+				{
+					caseTitle:"生成v字",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/5.png",
+					caseDescription:"点击生成4个方向的v字",
+					caseWebsite:"demo/case2/生成4个V.html"
+				},
+				{
+					caseTitle:"生成10个div",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/6.png",
+					caseDescription:"点击生成4个方向的v字",
+					caseWebsite:"demo/case2/生成10个div.html"
+				},
+				{
+					caseTitle:"生成100个div",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/7.png",
+					caseDescription:"生成100个div",
+					caseWebsite:"demo/case2/生成100个div.html"
+				},{
+					caseTitle:"生成九九乘法表",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/8.png",
+					caseDescription:"生成九九乘法表",
+					caseWebsite:"demo/case2/生成九九乘法表.html"
+				},
+				{
+					caseTitle:"生成新闻列表",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/9.png",
+					caseDescription:"生成新闻列表",
+					caseWebsite:"demo/case2/生成新闻列表.html"
+				},{
+					caseTitle:"橡皮擦",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/10.png",
+					caseDescription:"鼠标移入擦除图片上的遮罩层",
+					caseWebsite:"demo/case2/橡皮擦.html"
+				}
+			]
+		},{
+			caseName:"自定义属性、索引值",
+			caseList:[
+				{
+					caseTitle:"操作多组图片切换",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/11.png",
+					caseDescription:"点击上一组或下一组同时切换两组图片，点击图片分别切换图片",
+					caseWebsite:"demo/case3/操作多组图片切换.html"
+				},
+				{
+					caseTitle:"带缩略图图片切换",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/12.png",
+					caseDescription:"鼠标移入下边横条显示小图片，点击小图标切换图片，点击左右按钮切换图片",
+					caseWebsite:"demo/case3/带缩略图图片切换.html"
+				},
+				{
+					caseTitle:"仿qq列表",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/13.png",
+					caseDescription:"点击列表来回切换展开列表",
+					caseWebsite:"demo/case3/仿qq列表.html"
+				}
+			]
+		},
+		{
+			caseName:"JS数据类型、类型转换",
+			caseList:[
+				{
+					caseTitle:"qq号码判断",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/14.png",
+					caseWebsite:"demo/case4/qq号码判断.html"
+				},
+				{
+					caseTitle:"选数据类型",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/20.png",
+					caseWebsite:"demo/case4/选数据类型.html"
+				}
+			]
+		},{
+			caseName:"函数传参、重用、价格计算",
+			caseList:[
+				{
+					caseTitle:"嵌套选项卡图片切换",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/15.png",
+					caseDescription:"点击左边的选项卡切换图片，移入下边的选项卡切换图片",
+					caseWebsite:"demo/case5/嵌套选项卡图片切换.html"
+				},
+				{
+					caseTitle:"商品价格计算",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/16.png",
+					caseDescription:"计算商品总价和总量",
+					caseWebsite:"demo/case5/商品价格计算.html"
+				},
+				{
+					caseTitle:"搜狐",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/17.png",
+					caseDescription:"鼠标移入视频列表出现详情",
+					caseWebsite:"demo/case5/搜狐.html"
+				},
+				{
+					caseTitle:"图片切换（传参）",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/18.png",
+					caseWebsite:"demo/case5/图片切换（传参）.html"
+				},
+				{
+					caseTitle:"修改文本内容",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/19.png",
+					caseWebsite:"demo/case5/修改文本内容.html"
+				}
+			]
+		},
+		{
+			caseName:"运算符流程控制",
+			caseList:[
+				{
+					caseTitle:"百度音乐全选操作",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/21.png",
+					caseDescription:"百度音乐全选操作",
+					caseWebsite:"demo/case6/百度音乐全选操作.html"
+				},
+				{
+					caseTitle:"评分",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/22.png",
+					caseDescription:"鼠标移入小星星图标打分",
+					caseWebsite:"demo/case6/评分.html"
+				}
+			]
+		},{
+			caseName:"定时器管理、函数封装",
+			caseList:[
+				{
+					caseTitle:"淘宝广告效果",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/23.png",
+					caseDescription:"图片自动轮换切换，点击选项卡切换图片，停止自动轮播",
+					caseWebsite:"demo/case7/淘宝广告效果.html"
+				},
+				{
+					caseTitle:"图片自动切换",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/24.png",
+					caseWebsite:"demo/case7/图片自动切换.html"
+				},
+				{
+					caseTitle:"延时消失的菜单",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/25.png",
+					caseWebsite:"demo/case7/延时消失的菜单.html"
+				},
+				{
+					caseTitle:"页面弹出大广告",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/26.png",
+					caseWebsite:"demo/case7/页面弹出大广告.html"
+				},
+				{
+					caseTitle:"自动换背景",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/27.png",
+					caseWebsite:"demo/case7/自动换背景.html"
+				},
+				{
+					caseTitle:"自动轮换选项卡",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/28.png",
+					caseWebsite:"demo/case7/自动轮换选项卡.html"
+				},
+				{
+					caseTitle:"带详情的轮播图",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/29.png",
+					caseWebsite:"demo/case7/带详情的轮播图.html"
+				},
+				{
+					caseTitle:"自动轮播和鼠标切换图片",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/30.png",
+					caseWebsite:"demo/case7/自动轮播和鼠标切换图片.html"
+				}
+			]
+		},
+		{
+			caseName:"日期对象、时钟倒计时",
+			caseList:[
+				{
+					caseTitle:"图片时钟",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/31.png",
+					caseWebsite:"demo/case8/图片时钟.html"
+				},
+				{
+					caseTitle:"仿windows10日历",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/32.png",
+					caseWebsite:"demo/case8/仿windows10日历.html"
+				}
+			]
+		},{
+			caseName:"字符串、查找高亮显示",
+			caseList:[
+				{
+					caseTitle:"5-文字搬运工",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/33.png",
+					caseWebsite:"demo/case9/5-文字搬运工.html"
+				},
+				{
+					caseTitle:"排序",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/34.png",
+					caseWebsite:"demo/case9/排序.html"
+				},
+				{
+					caseTitle:"随机翻动",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/35.png",
+					caseWebsite:"demo/case9/随机翻动.html"
+				},
+				{
+					caseTitle:"查找替换",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/36.png",
+					caseWebsite:"demo/case9/查找替换.html"
+				}
+			]
+		},
+		{
+			caseName:"事件",
+			caseList:[
+				{
+					caseTitle:"按键控制键图片切换",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/37.png",
+					caseWebsite:"demo/case10/按键控制键图片切换.html"
+				},
+				{
+					caseTitle:"拖拽框",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/38.png",
+					caseDescription:"点击恢复div按原路自动返回",
+					caseWebsite:"demo/case10/拖拽框.html"
+				},
+				{
+					caseTitle:"吸附",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/39.png",
+					caseWebsite:"demo/case10/吸附.html"
+				},
+				{
+					caseTitle:"右键鼠标提示框",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/40.png",
+					caseWebsite:"demo/case10/右键鼠标提示框.html"
+				},
+				{
+					caseTitle:"搜索提示",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/42.png",
+					caseWebsite:"demo/case10/搜索提示.html"
+				}
+			]
+		},
+		{
+			caseName:"canvas 炫酷时钟",
+			caseList:[
+				{
+					caseTitle:"炫酷时钟",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/43.png",
+					caseWebsite:"demo/case11/time.html"
+				}
+		    ]
+		},
+		{
+			caseName:"jq、js 瀑布流",
+			caseList:[
+				{
+					caseTitle:"瀑布流js原生",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/44.png",
+					caseWebsite:"demo/case12/js原生1.html"
+				},
+				{
+					caseTitle:"瀑布流jq",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/44.png",
+					caseWebsite:"demo/case12/jq方式.html"
+				}
+		    ]
+		},
+		{
+			caseName:"BOM、DOM",
+			caseList:[
+				{
+					caseTitle:"search、hash的练习",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/45.png",
+					caseWebsite:"demo/case13/search、hash的练习/content.html"
+				},
+				{
+					caseTitle:"表格练习",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/46.png",
+					caseWebsite:"demo/case13/表格练习.html"
+				},
+				{
+					caseTitle:"多级菜单2",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/47.png",
+					caseWebsite:"demo/case13/多级菜单2.html"
+				},
+				{
+					caseTitle:"上移下移",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/48.png",
+					caseWebsite:"demo/case13/上移下移.html"
+				},
+				{
+					caseTitle:"生成tab1",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/49.png",
+					caseWebsite:"demo/case13/生成tab1.html"
+				},
+				{
+					caseTitle:"鼠标移入变换背景",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/50.png",
+					caseWebsite:"demo/case13/鼠标移入变换背景.html"
+				},
+				{
+					caseTitle:"招聘",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/51.png",
+					caseWebsite:"demo/case13/招聘.html"
+				},
+				{
+					caseTitle:"创建图标",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/52.png",
+					caseWebsite:"demo/case13/创建图标.html"
+				},
+				{
+					caseTitle:"鼠标移入显示提示详情",
+					publishTime:"2016-12-12 5:26",
+					caseThumbnail:"images/case/1/53.png",
+					caseWebsite:"demo/case13/详情.html"
+				}
+		    ]
+		}
+]
---
+++ data/project.js
@@ -0,0 +1,57 @@
+
+/*		项目池
+	*   {
+	*       projectName: @value String            *         项目名称
+	*       projectWebsite: @value String         *         项目网址
+	*       startTime: @value String              *         项目开始时间  格式为：2016.10.09
+	*       endTime: @value String 			      *         项目结束时间  格式为：2016.10.09
+	*       projectExplain: @value String         *         项目说明
+	*       projectLabel: @value Array            *         项目技术标签 
+			projectThumbnail: @value String       *         缩略图地址
+	*       
+	*   }
+*/
+
+var projects = [
+		{
+			projectName:"电影主题网站",
+			projectWebsite:"demo/case/1-电影主题整站/index.html",
+		    startTime:"2016.10.20",
+			endTime:"至今",
+			projectExplain:"JS整站开发之：页面中数据操作方面充分利用了js的功能。整屏切换页面，定时器管理，开关管理，运动函数，jQuery运用，JSON的数据交换原理的运用。",
+			projectLabel:["html","css3","css","js","jq"],
+			projectThumbnail:"images/icon/1.png"
+		},
+		{
+			projectName:"仿WebQQ桌面",
+			projectWebsite:"demo/case/2-WebQQ/index.html",
+		    startTime:"2016.06.01",
+			endTime:"至今",
+			projectExplain:"表单控件、整屏切换、DOM操作、背景切换",
+			projectLabel:["html","css3","css","js"],
+			projectThumbnail:"images/icon/project-2.png"
+		},{
+			projectName:"仿途牛静态页面",
+			projectWebsite:"demo/case/途牛整站/index.html",
+		    startTime:"2016.05.01",
+			endTime:"至今",
+			projectExplain:"静态页面布局，包括首页、分页",
+			projectLabel:["html","css"],
+			projectThumbnail:"images/icon/project-3.png"
+		},{
+			projectName:"仿途牛移动端",
+			projectWebsite:"demo/case/途牛移动端/index.html",
+		    startTime:"2016.05.10",
+			endTime:"至今",
+			projectLabel:["html","css"],
+			projectThumbnail:"images/icon/project-4.png"
+		},{
+			projectName:"仿响应式静态网页",
+			projectWebsite:"demo/case/响应式/xiang.html",
+		    startTime:"2016.06.01",
+			endTime:"至今",
+			projectExplain:"响应式静态页面",
+			projectLabel:["html","css"],
+			projectThumbnail:"images/icon/project.png"
+		}
+]
